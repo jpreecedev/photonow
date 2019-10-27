0f152fba324fc1372365f930a5c7d25ec210527c
Jon Preece <jonpreece@hotmail.co.uk>
2019-10-27 16:47:30 +0000

    feat(ga): added google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -42,6 +42,16 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-150996298-1"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-150996298-1');`
+            }}
+          />
         </body>
       </Html>
     )
