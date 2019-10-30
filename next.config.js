bb1c0d144264a011bb6687114635217ea12495ab
Jon Preece <jonpreece@hotmail.co.uk>
2019-10-30 19:27:04 +0000

    fix(sec): minimise exposure

--- next.config.js
+++ next.config.js
@@ -2,9 +2,16 @@ const { parsed: localEnv } = require("dotenv").config()
 const webpack = require("webpack")
 const withCSS = require("@zeit/next-css")
 
+const exposedConfig = {
+  GOOGLE_PLACES_API_KEY: localEnv.GOOGLE_PLACES_API_KEY,
+  BASE_API_URL: localEnv.BASE_API_URL,
+  STRIPE_PUBLISHABLE_KEY: localEnv.STRIPE_PUBLISHABLE_KEY,
+  SERVER_API_URL: localEnv.SERVER_API_URL
+}
+
 module.exports = withCSS({
   webpack(config, options) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+    config.plugins.push(new webpack.EnvironmentPlugin(exposedConfig))
     return config
   }
 })
