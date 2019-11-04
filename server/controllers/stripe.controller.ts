5d81b954dca76f16f51805eb18e95d8b6eee1fd1
Jon Preece <jonpreece@hotmail.co.uk>
2019-11-04 07:56:56 +0000

    feat(stripe): checkout WIP

--- server/controllers/stripe.controller.ts
+++ server/controllers/stripe.controller.ts
@@ -1,13 +1,61 @@
+import { Types } from "mongoose"
 import fetch from "isomorphic-unfetch"
 import uuid from "uuid/v4"
 import { Response, Request } from "express"
 import { to } from "await-to-js"
+import Stripe from "stripe"
 
 import { createCsrfToken } from "../database/stripecsrftoken"
-import { StripeCsrfToken, StripeExpressConnect, UserRequest } from "../../global"
+import {
+  StripeCsrfToken,
+  StripeExpressConnect,
+  UserRequest,
+  PictureItem,
+  ClientResponse
+} from "../../global"
 import { errorHandler } from "../utils"
 import { updateStripeData } from "../database/user/update"
 import { getRedirectUrl } from "../auth/utils"
+import { getMoments } from "../database/moments"
+import { getCollection } from "../database/collection"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "")
+
+async function createSession(req: Request, res: Response) {
+  try {
+    const { pictures }: { pictures: PictureItem[] } = req.body
+
+    const moments = await getMoments(pictures.map(picture => Types.ObjectId(picture.momentId)))
+    const collection = await getCollection(moments[0].collectionId)
+
+    const lineItems: Stripe.checkouts.sessions.ICheckoutLineItems[] = moments.map(moment => ({
+      name: moment.filename,
+      description: moment.filename,
+      images: [moment.resizedLocation],
+      amount: collection.price,
+      currency: "gbp",
+      quantity: 1
+    }))
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: lineItems,
+      success_url: `${process.env.SERVER_API_URL}/stripe/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.SERVER_API_URL}/stripe/cancel`
+    })
+
+    return res.status(200).json(<ClientResponse<string>>{
+      success: true,
+      data: session.id
+    })
+  } catch (e) {
+    errorHandler.handle(e)
+    return res.status(500).json(<ClientResponse<string>>{
+      success: false,
+      data: e
+    })
+  }
+}
 
 async function start(req: Request, res: Response) {
   const state = uuid()
@@ -62,4 +110,4 @@ async function requestAccess(req: UserRequest, res: Response) {
   return res.redirect(getRedirectUrl(updatedUser))
 }
 
-export default { start, requestAccess }
+export default { start, requestAccess, createSession }
--- server/router/stripe.routes.ts
+++ server/router/stripe.routes.ts
@@ -1,10 +1,22 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import passport from "passport"
+
 import { StripeController } from "../controllers"
 import { utils } from "../auth"
 import { ROLES } from "../../utils/roles"
 
 const router = express.Router()
+
+router.post("/session", StripeController.createSession)
+
+router.get("/success", (req: Request, res: Response) => {
+  const { session_id } = req.query
+  // TODO
+})
+
+router.get("/cancel", (req: Request, res: Response) => {
+  // TODO
+})
 
 router.get(
   "/start",
