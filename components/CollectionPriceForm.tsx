7be53bd178eaa1692e2d2f8f54652e6795d06861
Jon Preece <jonpreece@hotmail.co.uk>
2019-11-09 17:03:50 +0000

    fix(payment): set minimum price on collections

--- components/CollectionPriceForm.tsx
+++ components/CollectionPriceForm.tsx
@@ -1,16 +1,55 @@
 import React, { FunctionComponent } from "react"
 import { Field, reduxForm, InjectedFormProps } from "redux-form"
-import { Grid, InputAdornment } from "@material-ui/core"
+import { Grid, InputAdornment, CircularProgress, Button } from "@material-ui/core"
+import { makeStyles, Theme } from "@material-ui/core/styles"
 
 import { renderTextField } from "./ReduxForm"
 
-interface CollectionPriceFormProps {}
+interface CollectionPriceFormProps {
+  price: Number
+}
 
-const CollectionPriceForm: FunctionComponent<InjectedFormProps & CollectionPriceFormProps> = ({
-  handleSubmit
+const validate = (values: CollectionPriceFormProps) => {
+  const errors = {
+    price: null
+  }
+
+  const requiredFields = ["price"]
+  requiredFields.forEach(field => {
+    if (!values[field]) {
+      errors[field] = "Required"
+    } else if (Number.parseInt(values[field]) === Number.NaN) {
+      errors[field] = "Required"
+    } else if (Number.parseInt(values[field]) < 100) {
+      errors[field] = "Must be at least 100"
+    }
+  })
+
+  return errors
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  button: {
+    marginTop: theme.spacing(1)
+  },
+  buttonProgress: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -12,
+    marginLeft: -12
+  }
+}))
+
+const CollectionPriceForm: FunctionComponent<InjectedFormProps<CollectionPriceFormProps>> = ({
+  handleSubmit,
+  submitting,
+  valid
 }) => {
+  const classes = useStyles({})
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form noValidate onSubmit={handleSubmit}>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
           <Field
@@ -26,12 +65,22 @@ const CollectionPriceForm: FunctionComponent<InjectedFormProps & CollectionPrice
           />
         </Grid>
       </Grid>
+      <Button
+        disabled={submitting || !valid}
+        variant="contained"
+        className={classes.button}
+        onClick={handleSubmit}
+      >
+        {submitting && <CircularProgress size={24} className={classes.buttonProgress} />}
+        Update Price
+      </Button>
     </form>
   )
 }
 
 const ReduxCollectionPriceForm = reduxForm<CollectionPriceFormProps>({
-  form: "collectionPriceForm"
+  form: "collectionPriceForm",
+  validate
 })(CollectionPriceForm)
 
 export { ReduxCollectionPriceForm as CollectionPriceForm }
--- components/CollectionPriceFormContainer.tsx
+++ components/CollectionPriceFormContainer.tsx
@@ -1,38 +1,22 @@
 import React, { FunctionComponent } from "react"
 import { Types } from "mongoose"
 import { FormState } from "redux-form"
-import { CircularProgress, Button } from "@material-ui/core"
 import { connect, DispatchProp } from "react-redux"
-import { makeStyles, Theme } from "@material-ui/core/styles"
 
 import { AppState } from "../global"
 import { CollectionPriceForm } from "./CollectionPriceForm"
 import * as server from "../utils/server"
 import { Notification } from "./Notification"
 
-interface CollectionPriceFormContainer {
+interface CollectionPriceFormContainerProps {
   price: number
   collectionId: Types.ObjectId
   collectionPriceForm: FormState
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
-  button: {
-    marginTop: theme.spacing(1)
-  },
-  buttonProgress: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    marginTop: -12,
-    marginLeft: -12
-  }
-}))
-
 const CollectionPriceFormContainer: FunctionComponent<
-  CollectionPriceFormContainer & DispatchProp<any>
+  CollectionPriceFormContainerProps & DispatchProp<any>
 > = ({ collectionId, price, collectionPriceForm }) => {
-  const classes = useStyles({})
   const [processing, setProcessing] = React.useState(false)
   const [notification, setNotification] = React.useState({ open: false, message: "" })
 
@@ -62,16 +46,7 @@ const CollectionPriceFormContainer: FunctionComponent<
         message={notification.message}
         onClose={() => setNotification({ open: false, message: "" })}
       />
-      <CollectionPriceForm initialValues={{ price }} onSubmit={() => handleSubmit()} />
-      <Button
-        disabled={processing}
-        variant="contained"
-        className={classes.button}
-        onClick={handleSubmit}
-      >
-        {processing && <CircularProgress size={24} className={classes.buttonProgress} />}
-        Update Price
-      </Button>
+      <CollectionPriceForm initialValues={{ price }} onSubmit={handleSubmit} />
     </>
   )
 }
